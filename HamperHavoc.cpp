2611b1574701012444db68abf713c3c6c9fd1191
deanbunn <deanbunn@gmail.com>
2023-01-12 19:49:39 -0800

    20230112_1949hrs Pretty stuff

--- HamperHavoc.cpp
+++ HamperHavoc.cpp
@@ -2,8 +2,23 @@
 
 int main()
 {
+
+    //Say it loud
     std::cout << "\nHamper Havoc!\n\n";
-  
+    
+    //Declare Test Number
+    int nDtest {0};
+
+    //Ask How Many Hampers
+    std::cout << "Enter how many hampers to wash: ";
+    std::cin >> nDtest;
+
+    //Display Hamper Report
+    std::cout << "\nWe washed " << nDtest << " hampers";
+
+    //Clean view at the end
+    std::cout << "\n\n";
+
     return 0;
 }
 
