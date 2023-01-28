47fefd7a0c48f1a72a3de937c25c0d8da4c3ff37
deanbunn <deanbunn@gmail.com>
2023-01-27 19:02:59 -0800

    20230127_1902hrs Report Message

--- HamperHavoc.cpp
+++ HamperHavoc.cpp
@@ -1,20 +1,48 @@
 #include <iostream>
+#include <string>
+
 
 int main()
 {
 
+    //Declare Hamper Number
+    int nHampers{0};
+
+    //Declare Hamper Report Message
+    std::string sHamperMsg{""};
+
     //Say it loud
     std::cout << "\nHamper Havoc!\n\n";
     
-    //Declare Test Number
-    int nDtest {0};
-
     //Ask How Many Hampers
     std::cout << "Enter how many hampers to wash: ";
-    std::cin >> nDtest;
+    std::cin >> nHampers;
+
+    //Determine Hamper Wash Message
+    if(nHampers <= 5)
+    {
+       sHamperMsg = "That's an easy and nice wash day!"; 
+    }
+    else if(nHampers <= 10)
+    {
+       sHamperMsg = "Wow! That's a full load.";
+    }
+    else 
+    {
+       
+       sHamperMsg = "Did you only do laundry once a year???";
+
+       //Alert on Major Washing
+       for(int i{0}; i < nHampers; i++)
+       {
+          std::cout << "Brah! ";
+       }
+
+    }
 
     //Display Hamper Report
-    std::cout << "\nWe washed " << nDtest << " hampers";
+    std::cout << "\nWe washed " << nHampers << " hampers.\n\n";
+    std::cout << sHamperMsg;
 
     //Clean view at the end
     std::cout << "\n\n";
